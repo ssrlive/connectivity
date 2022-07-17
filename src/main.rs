27b50d134ef454febcd3d991f1498d5711f2542a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-07-17 19:33:55 +0800

    404 page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 #[macro_use]
 extern crate rocket;
+use rocket::Request;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _ = rocket::build().mount("/", routes![index]).launch().await?;
+    let _ = rocket::build()
+        .register("/", catchers![not_found])
+        .mount("/", routes![index])
+        .launch()
+        .await?;
     Ok(())
 }
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
 }
