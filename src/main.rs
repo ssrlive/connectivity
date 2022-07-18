4e25682ee397663792287567a78c3fa97a0e8401
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-07-18 18:51:27 +0800

    ping finished

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #[macro_use]
 extern crate rocket;
-use rocket::Request;
+use port_check::is_port_reachable_with_timeout;
+use rocket::{serde::json::Json, Request};
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let _ = rocket::build()
         .register("/", catchers![not_found])
-        .mount("/", routes![index])
+        .mount("/", routes![index, ping])
         .launch()
         .await?;
     Ok(())
@@ -20,4 +23,38 @@ fn index() -> &'static str {
 #[catch(404)]
 fn not_found(req: &Request) -> String {
     format!("Sorry, '{}' is not a valid path.", req.uri())
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct TargetAddr {
+    host: String,
+    port: u16,
+}
+
+impl TargetAddr {
+    fn to_host_port(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+
+    fn is_reachable(&self) -> bool {
+        is_port_reachable_with_timeout(self.to_host_port(), Duration::from_millis(10_000))
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct PingResult {
+    target: TargetAddr,
+    result: bool,
+}
+
+#[get("/ping?<host>&<port>")]
+async fn ping(host: &str, port: u16) -> Json<PingResult> {
+    let target = TargetAddr {
+        host: host.to_string(),
+        port,
+    };
+    let result = target.is_reachable();
+    Json(PingResult { target, result })
 }
